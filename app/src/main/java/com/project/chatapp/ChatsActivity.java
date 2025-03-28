04b60cdada0dc870958a9cff4a48cb934701eccb
Nguyễn Ngọc Thịnh <thinh23022004@gmail.com>
2025-03-28 17:31:28 +0700

    str

--- app/src/main/java/com/project/chatapp/ChatsActivity.java
+++ app/src/main/java/com/project/chatapp/ChatsActivity.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.project.chatapp.databinding.ActivityChatsBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatsActivity extends AppCompatActivity {
     private ActivityChatsBinding binding ;
+    private List<StoryModel> listStory ;
+    private CustomAdapterRVStory adapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityChatsBinding.inflate(getLayoutInflater()) ;
         setContentView(binding.getRoot());
+
+        listStory = new ArrayList<>() ;
+        listStory.add(new StoryModel(R.drawable.pic1,"tom")) ;
+        listStory.add(new StoryModel(R.drawable.pic2,"meo")) ;
+        listStory.add(new StoryModel(R.drawable.pic3,"cho")) ;
+
+        adapter = new CustomAdapterRVStory(listStory);
+        binding.rvStory.setLayoutManager(new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false));
+        binding.rvStory.setAdapter(adapter);
 
     }
 }
